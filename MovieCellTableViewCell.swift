99e13aadbee9ace38091958dedc146e953dbbcee
Your Name <you@example.com>
2019-02-23 19:52:28 -0500

    first commit

---
+++ MovieCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCellTableViewCell.swift
+//  flister
+//
+//  Created by Amadou Diallo on 2/20/19.
+//  Copyright © 2019 code path. All rights reserved.
+//
+
+import UIKit
+
+class MovieCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'flister' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for flister
+pod "AlamofireImage"
+
+  target 'flisterTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'flisterUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
